f818acaf27f10ea2bfd7316ac064c8180ab73143
Alemew <59649239+Alemew@users.noreply.github.com>
2022-01-10 20:20:42 +0100

    Clase 10/1 casa

--- Assets/Script/AIEnemy.cs
+++ Assets/Script/AIEnemy.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 public class AIEnemy : MonoBehaviour
 {
@@ -14,6 +17,53 @@ public class AIEnemy : MonoBehaviour
 
     [SerializeField] 
     private EnemyState _currentState = EnemyState.PATROL;
+
+    public EnemyState CurrentState
+    {
+        get { return _currentState; }
+        set
+        {
+            _currentState = value;
+            StopAllCoroutines();
+            switch (value)
+            {
+                case EnemyState.PATROL :
+                {
+                    StartCoroutine(AIPatrol());
+                    break;
+                }
+                case EnemyState.CHASE :
+                {
+                    StartCoroutine(AIChase());
+                    break;
+                }
+                case EnemyState.ATTACK :
+                {
+                    StartCoroutine(AIAttack());
+                    break;
+                }
+            }
+        }
+    }
+
+    private LineSight _theLineSight;
+    private NavMeshAgent _theAgent;
+    private Transform target;
+    private Transform destination;
+
+    private void Awake()
+    {
+        _theLineSight = GetComponent<LineSight>();
+        _theAgent = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    private void Start()
+    {
+        CurrentState = EnemyState.PATROL;
+        GameObject[] randomDestinations = GameObject.FindGameObjectsWithTag("Destination");
+        destination = randomDestinations[Random.Range(0, randomDestinations.Length)].GetComponent<Transform>();
+    }
 
     public IEnumerator AIPatrol()
     {
